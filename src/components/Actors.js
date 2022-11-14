27fd8843aa41df24999ccedf29fcd5aaf77fbbea
Antonio <antonio18casta@aol.com>
2022-11-14 14:06:44 -0500

    commit tree f237bf745426aaae01feb7c014a8dc66b5cdf689

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const getActors = actors.map((actor) => (
+    <div key={actor.name}>
+      <h2>Name: {actor.name}</h2>
+      Movies:
+      <ul>
+        {actor.movies.map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+
+  return (
+    <div>
+      <h1>Actors Pages</h1>
+      {getActors}
+    </div>
+  )
 }
 
 export default Actors;
